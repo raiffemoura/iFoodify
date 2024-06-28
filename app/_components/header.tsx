92b5db39bcda8b12afe79a1fda1e1e37fb280f5d
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2024-06-28 12:53:32 -0300

    style: improe logo quality on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-6 py-5">
-      <Image src="/logo.png" alt="iFoodfy" height={30} width={100} />
+      <div className="relative h-[40px] w-[120px]">
+        <Link href={"/"}>
+          <Image src="/logo.png" alt="iFoodfy" fill className="object-cover" />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
